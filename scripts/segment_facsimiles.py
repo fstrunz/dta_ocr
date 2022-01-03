982223d2ca3984e890bed33895140c95ca768b04
Florian Strunz <florian.strunz@stud-mail.uni-wuerzburg.de>
2022-01-03 11:06:31 +0100

    Binarisation & deskewing using OCRopus

--- scripts/segment_facsimiles.py
+++ scripts/segment_facsimiles.py
@@ -1,36 +1,107 @@
 import argparse
 import multiprocessing
+import subprocess
+import os
+import itertools
 from pathlib import Path
-from typing import Iterable, Tuple
-from PIL import Image
-from kraken import binarization
+from typing import Iterable, List, Tuple, TypeVar
+
+T = TypeVar("T")
+
+
+def filename_without_exts(path: Path) -> str:
+    filename = path.name
+    dot_index = filename.index(".")
+    return filename[:dot_index]
+
+
+def chunked(iterable: Iterable[T], n: int) -> Iterable[List[T]]:
+    it = iter(iterable)
+
+    while True:
+        # take next n elements
+        chunk = list(itertools.islice(it, n))
+
+        # we are done
+        if not chunk:
+            return
+
+        yield chunk
 
 
 def facsimiles(facsimile_path: Path) -> Iterable[Tuple[Path, Path]]:
     for doc_path in facsimile_path.iterdir():
         if doc_path.is_dir():
             for fac_path in doc_path.iterdir():
-                if fac_path.is_file():
+                if (
+                    fac_path.is_file() and
+                    not fac_path.name.endswith(".bin.png") and
+                    not fac_path.name.endswith(".nrm.png")
+                ):
                     yield fac_path, doc_path
 
 
-def binarise_facsimile(inp: Tuple[Path, Path]):
-    fac_path, parent_dir = inp
+def binarise_facsimile_chunk(
+    ocropy_venv: Path, chunk_paths: List[Path], process_count: int
+) -> bool:
+    python_path = (ocropy_venv / "bin" / "python").resolve()
+    nlbin_path = (ocropy_venv / "bin" / "ocropus-nlbin").resolve()
+    arguments = [python_path, nlbin_path]
+    arguments += chunk_paths
 
-    print(f"Binarising {fac_path}...")
-    bin_dir: Path = parent_dir / "bin"
-    bin_dir.mkdir(exist_ok=True)
+    with subprocess.Popen(arguments) as nlbin_proc:
+        exit_code = nlbin_proc.wait()
+        if exit_code:
+            print(
+                f"ocropy-nlbin failed with exit code {exit_code}, " +
+                f"given arguments {arguments}"
+            )
+            return False
 
-    # binarise image and save it in the bin/ dir
-    # under the same name
-    fac_img = Image.open(fac_path)
-    bin_img = binarization.nlbin(fac_img)
-    bin_img.save(bin_dir / fac_path.name)
+    for fac_path in chunk_paths:
+        bin_dir = fac_path.parent / "bin"
+        bin_dir.mkdir(exist_ok=True)
+
+        name = filename_without_exts(fac_path)
+        bin_path = fac_path.parent / f"{name}.bin.png"
+        nrm_path = fac_path.parent / f"{name}.nrm.png"
+
+        if bin_path.is_file():
+            bin_path.rename(bin_dir / bin_path.name)
+        else:
+            print(f"Bin file \"{bin_path}\" not found!!")
+
+        if nrm_path.is_file():
+            nrm_path.rename(bin_dir / nrm_path.name)
+        else:
+            print(f"Nrm file \"{nrm_path}\" not found!!")
+
+    return True
 
 
-def binarise_facsimiles(facsimile_path: Path, process_count: int):
+def binarise_facsimiles(
+    ocropy_venv: Path, facsimile_path: Path, process_count: int
+) -> bool:
+    facsimile_count = sum(1 for _ in facsimiles(facsimile_path))
+
+    chunk_size = facsimile_count // process_count
+    print(f"Processing {facsimile_count} in {chunk_size} chunks " +
+          f"over {process_count} processes...")
+
     with multiprocessing.Pool(process_count) as pool:
-        pool.map(binarise_facsimile, facsimiles(facsimile_path))
+        pool.starmap(
+            binarise_facsimile_chunk,
+            ((ocropy_venv, chunk, process_count) for chunk in chunked(
+                (fac for fac, _ in facsimiles(facsimile_path)),
+                facsimile_count // process_count
+            ))
+        )
+
+
+def preprocess_facsimiles(
+    facsimile_path: Path, process_count: int, ocropy_venv: Path
+):
+    binarise_facsimiles(ocropy_venv, facsimile_path, process_count)
 
 
 def main():
@@ -42,6 +113,13 @@ def main():
             "Given a set of facsimiles, in the same format as the output " +
             "of download_facsimiles.py, preprocesses (binarisation, " +
             "deskewing) and segments all images."
+        )
+    )
+    arg_parser.add_argument(
+        "--ocropy-venv", dest="ocropy_venv", required=True,
+        help=(
+            "Path to a virtualenv containing a Python 2.7 install along with" +
+            "the OCRopy binaries."
         )
     )
     arg_parser.add_argument(
@@ -62,12 +140,30 @@ def main():
     )
     args = arg_parser.parse_args()
 
+    ocropy_venv = Path(args.ocropy_venv)
+    if not ocropy_venv.is_dir():
+        print("Given OCRopy venv directory is not a directory!")
+        exit(1)
+
+    python_path = ocropy_venv / "bin" / "python"
+    if not python_path.is_file():
+        print("Given OCRopy venv does not have a Python binary!")
+        exit(1)
+
+    if not os.access(python_path, os.X_OK):
+        print("Given OCRopy venv Python binary is not executable!")
+        exit(1)
+
+    if not (ocropy_venv / "bin" / "ocropus-nlbin").is_file():
+        print("Given OCRopy venv does not have an ocropus-nlbin script!")
+        exit(1)
+
     facsimile_path = Path(args.facsimile_dir)
     if not facsimile_path.is_dir():
         print("Given facsimile directory is not a directory!")
         exit(1)
 
-    binarise_facsimiles(facsimile_path, args.process_count)
+    preprocess_facsimiles(facsimile_path, args.process_count, ocropy_venv)
 
 
 if __name__ == "__main__":
