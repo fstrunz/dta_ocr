8768961c0d3a603f1ebdf98aec627633a84940b7
Florian Strunz <florian.strunz@stud-mail.uni-wuerzburg.de>
2022-03-06 20:46:44 +0100

    Start reading TEI files using tei-reader

--- match.py
+++ match.py
@@ -1,6 +1,69 @@
 import argparse
 import sqlite3
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List
 from dta_ocr import create_progress_schema
+from tei_reader import TeiReader
+from page.elements import PcGts, TextRegion
+
+
+def schedule_matchings(conn: sqlite3.Connection):
+    cursor = conn.execute(
+        """SELECT dta_dirname, page_number
+        FROM predictions
+        WHERE status = 'finished'"""
+    )
+
+    conn.executemany(
+        """INSERT OR IGNORE INTO matchings
+            ( dta_dirname, page_number, gt_path, status )
+        VALUES
+            ( ?, ?, NULL, 'pending' )""",
+        cursor
+    )
+    conn.commit()
+
+
+@dataclass
+class Matching:
+    dta_dirname: str
+    page_number: int
+    pred_path: Path
+    tei_path: Path
+
+
+def fetch_scheduled_matchings(conn: sqlite3.Connection) -> List[Matching]:
+    cursor = conn.execute(
+        """SELECT m.dta_dirname, m.page_number, p.prediction_path, d.tei_path
+        FROM matchings m
+        NATURAL JOIN predictions p
+        JOIN documents d ON m.dta_dirname = d.dta_dirname
+        WHERE m.status != 'finished'
+        ORDER BY m.dta_dirname, m.page_number"""
+    )
+    return [
+        Matching(dta_dirname, page_number, Path(pred_path), Path(tei_path))
+        for dta_dirname, page_number, pred_path, tei_path in cursor
+    ]
+
+
+def match(matching: Matching):
+    with matching.pred_path.open("r") as file:
+        pcgts = PcGts.from_file(file)
+
+    page = pcgts.page
+    pred_lines: List[str] = [
+        line.unicode
+        for region in page.regions
+        if isinstance(region, TextRegion)
+        for line in region.lines
+        if line.text is not None
+    ]
+
+    tei_reader = TeiReader()
+    tei = tei_reader.read_file(str(matching.tei_path))
+    gt_text: str = tei.text
 
 
 def main():
@@ -9,7 +72,7 @@ def main():
         description=(
             "Given a set of predictions generated with predict.py, " +
             "attempts to match the predicted lines to sections in " +
-            "the original TEI file."
+            "the original TEI file, and outputs a ground truth."
         )
     )
     arg_parser.add_argument(
--- schema.sql
+++ schema.sql
@@ -61,7 +61,7 @@ CREATE TABLE IF NOT EXISTS predictions (
     ),
     status TEXT CHECK (
         status IN ( 'pending', 'error', 'finished' )
-    ),
+    ) NOT NULL,
     PRIMARY KEY ( dta_dirname, page_number ),
     FOREIGN KEY ( dta_dirname, page_number )
         REFERENCES segmentations ( dta_dirname, page_number )
@@ -69,7 +69,7 @@ CREATE TABLE IF NOT EXISTS predictions (
             ON UPDATE NO ACTION
 );
 
-/*CREATE TABLE IF NOT EXISTS matchings (
+CREATE TABLE IF NOT EXISTS matchings (
     dta_dirname TEXT NOT NULL,
     page_number INTEGER CHECK ( page_number >= 1 ) NOT NULL,
     gt_path TEXT CHECK (
@@ -78,16 +78,16 @@ CREATE TABLE IF NOT EXISTS predictions (
     ),
     status TEXT CHECK (
         status IN ( 'pending', 'error', 'finished' )
-    ),
+    ) NOT NULL,
     PRIMARY KEY ( dta_dirname, page_number ),
     FOREIGN KEY ( dta_dirname, page_number )
-        REFERENCES predictions ( dta_dirname )
+        REFERENCES predictions ( dta_dirname, page_number )
             ON DELETE CASCADE
             ON UPDATE NO ACTION
-);*/
+);
 
 CREATE UNIQUE INDEX IF NOT EXISTS documents_pkey_index ON documents ( dta_dirname );
 CREATE UNIQUE INDEX IF NOT EXISTS facsimiles_pkey_index ON facsimiles ( dta_dirname, page_number );
 CREATE UNIQUE INDEX IF NOT EXISTS segmentations_pkey_index ON segmentations ( dta_dirname, page_number );
 CREATE UNIQUE INDEX IF NOT EXISTS predictions_pkey_index ON predictions ( dta_dirname, page_number );
-/* CREATE UNIQUE INDEX IF NOT EXISTS matchings_pkey_index ON matchings ( dta_dirname, page_number ); */
+CREATE UNIQUE INDEX IF NOT EXISTS matchings_pkey_index ON matchings ( dta_dirname, page_number );
